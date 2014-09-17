70390e2b274a120ffb61c05ea901164b3385f7cf
Daniel Salsbach <daniel@linux-e7vk.site>
2014-09-17 13:33:15 +0200

    added prelim action class

--- CZHV/Crouching Zombie Hidden Viking The Game/src/controller/ControlListener.java
+++ CZHV/Crouching Zombie Hidden Viking The Game/src/controller/ControlListener.java
@@ -2,4 +2,5 @@ package controller;
 
 public interface ControlListener {
 	public void input(String key);
+	public void doAction(Action a);
 }
--- CZHV/Crouching Zombie Hidden Viking The Game/src/model/Game.java
+++ CZHV/Crouching Zombie Hidden Viking The Game/src/model/Game.java
@@ -1,21 +1,38 @@
 package model;
 
+import controller.Action;
 import controller.ControlListener;
 import model.map.Map;
 
-public class Game implements ControlListener{
+public class Game implements ControlListener {
 	Map map;
 	Character[] characters;
 	
-	public Game(){
+	public Game() {
 		
 	}
 
 	@Override
 	public void input(String key) {
-		switch(key){
-			
-		}
+		//intentionally left blank
 	}
-	
+
+	@Override
+	public void doAction(Action a) {
+		switch(a.getActionType()) {
+			case move:
+				
+				break;
+			case shoot:
+				
+				break;
+			case pickup:
+				
+				break;
+			case drop:
+				
+				break;
+			default: break;
+		}
+	}	
 }
