0769cbe39052b0685ee2fe71d71aa6ef568945f9
Daniel Salsbach <daniel@linux-e7vk.site>
2014-10-14 17:52:44 +0200

    final selection fix

--- CZHV/Crouching Zombie Hidden Viking The Game/src/controller/actions/ShootAction.java
+++ CZHV/Crouching Zombie Hidden Viking The Game/src/controller/actions/ShootAction.java
@@ -2,44 +2,41 @@ package controller.actions;
 
 import model.Game;
 import model.character.GameCharacter;
-import model.character.ItemSlot;
 import model.item.Weapon;
 
 public class ShootAction implements Action {
-	GameCharacter c1, c2;
-	float accuracy = 1;	//accuracy
-
-	public ShootAction(GameCharacter c1, GameCharacter c2) {
-		this.c1 = c1;
-		this.c2 = c2;
+	private Weapon w;
+	private GameCharacter target;
+	private GameCharacter source;
+	private int accuracy;
+	
+	public ShootAction(Weapon w, GameCharacter source, GameCharacter target){
+		this.w = w;
+		this.target = target;
+		this.source = source;
 	}
 	
-
-	public ShootAction(GameCharacter c1, GameCharacter c2, float accuracy){
-		this(c1,c2);
+	public ShootAction(Weapon w, GameCharacter source, GameCharacter target, int accuracy){
+		this.w = w;
+		this.target = target;
 		this.accuracy = accuracy;
+		this.source = source;
 	}
-
+	
+	
 	@Override
 	public boolean perform(Game g) {
-		int appliedDamage = 0;
-		ItemSlot[] inventory = c1.getBag().getInventory();
-		for (int i = 0; i < inventory.length; i++) {
-			if(inventory[i].getItem() instanceof Weapon){
-				Weapon w = (Weapon) inventory[i].getItem();
-				appliedDamage = w.getPower();
-				if(w.isMeleeWeapon()){
-					appliedDamage += c1.getStrength();
-				}
-				if(hitSuccess()){
-					c2.applyDamage(appliedDamage);
-				}
-				return true;
-			}
+		int appliedDamage = w.getPower();
+		if(w.isMeleeWeapon()){
+			appliedDamage += source.getStrength();
+		}
+		if(hitSuccess()){
+			target.applyDamage(appliedDamage);
+			return true;
 		}
 		return false;
 	}
-	
+
 	private boolean hitSuccess(){
 		return Math.random() < accuracy;
 	}
