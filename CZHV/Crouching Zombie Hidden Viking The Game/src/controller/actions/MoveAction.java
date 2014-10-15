c45324ac1d26818aa9a82fbd9e6ce7d9be5988e9
huib <huibdonkers@aol.com>
2014-10-15 14:15:13 +0200

    performance improvement: pathfinding is done in constructor of MoveAction, which is executed outside the main thread

--- CZHV/Crouching Zombie Hidden Viking The Game/src/controller/actions/MoveAction.java
+++ CZHV/Crouching Zombie Hidden Viking The Game/src/controller/actions/MoveAction.java
@@ -1,22 +1,30 @@
 package controller.actions;
 
+import java.util.List;
+
+import pathfinding.Node;
 import model.Game;
 import model.character.GameCharacter;
 
 
 public class MoveAction implements Action{
 	private GameCharacter c;
-	private float x,y;
+	private List<Node> path;
 	
 	public MoveAction(GameCharacter c, float x, float y){
 		this.c = c;
-		this.x = x;
-		this.y = y;
+		
+		this.path = c.getPathFinder().calculatePath(
+				c.getCell().getX(),
+				c.getCell().getY(),
+				(int)Math.round(x),
+				(int)Math.round(y)
+			);
 	}
 
 	@Override
 	public boolean perform(Game g) {
-		c.moveTo(x, y);
+		c.followPath(this.path);
 		return true;
 	}
 }
